de0a04f668c949c5118a407c64ce7b9a2e1887db
samarth kulshrestha <samarthkulshrestha@protonmail.com>
2023-05-11 16:50:32 +0530

    Improved code readability

--- src/main.cpp
+++ src/main.cpp
@@ -8,35 +8,29 @@
 #pragma warning(disable: 4996) //  fopen() deprecation
 #endif
 
-#define _USE_MATH_DEFINES#include <math.h>
+#define _USE_MATH_DEFINES
 
+#include <math.h>
 #include <stdint.h>
-
 #include <stdio.h>
-
 #include <string>
-
 #include <vector>
-
 #include <complex>
-
 #include <algorithm>
-
 #include <memory>
-
 #include <iostream>
 
 using std::cout;
 using std::endl;
 using std::complex;
 
-#if USE_CHRONO#include <chrono>
-
-#else#include <time.h>
-
+#if USE_CHRONO
+#include <chrono>
+#else
+#include <time.h>
 #endif
 
-namespace Calculate_Pi {
+namespace Calculate {
     uint32_t rand_word() {
         return (uint32_t)(
             (rand() & 0xf) << 0 |
@@ -88,8 +82,7 @@ namespace Calculate_Pi {
         #endif
     }
 
-    void dump_to_file(const char * path,
-        const std::string & str) {
+    void dump_to_file(const char* path, const std::string & str) {
         //  Dump a string to a file.
         FILE * file = fopen(path, "wb");
         if (file == NULL)
@@ -1005,6 +998,90 @@ namespace Calculate_Pi {
         return (x + .5) * (log(x) - 1) + (1.4189385332046727417803297364056176398613974736378);
     }
 
+    size_t e_terms(size_t p){
+        //  Returns the # of terms needed to reach a precision of p.
+
+        //  The taylor series converges to log(x!) / log(10) decimal digits after
+        //  x terms. So to find the number of terms needed to reach a precision of p
+        //  we need to solve this question for x:
+        //      p = log(x!) / log(1000000000)
+
+        //  This function solves this equation via binary search.
+
+        double sizeL = (double)p * 20.723265836946411156161923092159277868409913397659 + 1;
+
+        size_t a = 0;
+        size_t b = 1;
+
+        //  Double up
+        while (logf_approx((double)b) < sizeL)
+            b <<= 1;
+
+        //  Binary search
+        while (b - a > 1){
+            size_t m = (a + b) >> 1;
+
+            if (logf_approx((double)m) < sizeL)
+                a = m;
+            else
+                b = m;
+        }
+
+        return b + 2;
+    }
+
+    void e_BSR(BigFloat &P, BigFloat &Q, uint32_t a, uint32_t b){
+        //  Binary Splitting recursion for exp(1).
+
+        if (b - a == 1){
+            P = BigFloat(1);
+            Q = BigFloat(b);
+            return;
+        }
+
+        uint32_t m = (a + b) / 2;
+
+        BigFloat P0, Q0, P1, Q1;
+
+        e_BSR(P0, Q0, a, m);
+        e_BSR(P1, Q1, m, b);
+
+        P = P0.mul(Q1).add(P1);
+        Q = Q0.mul(Q1);
+    }
+
+    void e(size_t digits){
+        //  The leading 2 doesn't count.
+        digits++;
+
+        size_t p = (digits + 8) / 9;
+        size_t terms = e_terms(p);
+
+        //  Limit Exceeded
+        if ((uint32_t)terms != terms)
+            throw "Limit Exceeded";
+
+        cout << "Computing e..." << endl;
+        cout << "Algorithm: Taylor Series of exp(1)" << endl << endl;
+
+        double time0 = wall_clock();
+
+        cout << "Summing Series... " << terms << " terms" << endl;
+        BigFloat P, Q;
+        e_BSR(P, Q, 0, (uint32_t)terms);
+        double time1 = wall_clock();
+        cout << "Time: " << time1 - time0 << endl;
+
+        cout << "Division... " << endl;
+        P = P.div(Q, p).add(BigFloat(1), p);
+        double time2 = wall_clock();
+        cout << "Time: " << time2 - time1 << endl;
+
+        cout << "Total Time = " << time2 - time0 << endl << endl;
+
+        dump_to_file("e.txt", P.to_string(digits));
+    }
+
     //  pi
     void Pi_BSR(BigFloat & P, BigFloat & Q, BigFloat & R, uint32_t a, uint32_t b, size_t p) {
         //  Binary Splitting recursion for the Chudnovsky Formula.
@@ -1082,17 +1159,18 @@ namespace Calculate_Pi {
 
         cout << "Total Time = " << time4 - time0 << endl << endl;
 
-        /* cout << P.to_string(digits) << endl << endl; */
-        cout << "done" << endl;
+        // cout << P.to_string(digits) << endl << endl;
+        dump_to_file("pi.txt", P.to_string(digits));
+        cout << "Done." << endl;
     }
-} //  Namespace: Calculate_Pi
+}
 
 
 int main() {
     size_t digits = 1000000;
 
-    /* Calculate_Pi::e (digits); */
-    Calculate_Pi::Pi(digits);
+    // Calculate::e(digits);
+    Calculate::Pi(digits);
 
     #ifdef _WIN32
     system("pause");
